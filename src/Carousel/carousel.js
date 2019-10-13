3b4b3ecb0f7933e26a3fc28207a1b187761959a3
cddraper <clayddraper@gmail.com>
2019-10-13 16:53:34 -0500

    added carousel directory and files

---
+++ src/Carousel/carousel.js
@@ -0,0 +1,9 @@
+// External Dependencies
+import React from 'react';
+import styled from 'styled-components';
+
+// Local Dependencies
+import picOne from '../assets/photos/picOne.jpg';
+import picTwo from '../assets/photos/picTwo.jpg';
+import picThree from '../assets/photos/picThree.jpg';
+
