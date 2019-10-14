1f02ae8e273f102516ffa794ec49abc73a3a2e19
cddraper <clayddraper@gmail.com>
2019-10-13 20:05:38 -0500

    add carousel styling, add logic for buttons

--- src/Carousel/carouselButton.js
+++ src/Carousel/carouselButton.js
@@ -1,3 +1,26 @@
 // External Dependencies
 import React from 'react';
 import styled from 'styled-components';
+
+
+const StyledButton = styled.button`
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    outline: none;
+    border: 2px solid gray;
+
+    &:active {
+        background: darkgray;
+    }
+`
+
+// Component Definition
+const CarouselButton = (props) => {
+    return (
+        <StyledButton onClick={props.onClickCTA} />
+    )
+}
+
+
+export default CarouselButton;
--- src/Carousel/index.js
+++ src/Carousel/index.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 import picOne from '../assets/photos/picOne.jpg';
 import picTwo from '../assets/photos/picTwo.jpg';
 import picThree from '../assets/photos/picThree.jpg';
+import CarouselButton from './carouselButton';
 
 
 const Wrapper = styled.div({
@@ -14,26 +15,45 @@ const Wrapper = styled.div({
     height: '50vh',
     justifyContent: 'space-around',
     alignItems: 'center',
+    margin: 'auto',
+    marginTop: '2em',
 });
 
 const StyledImg = styled.img({
     maxHeight: '100%',
+    borderRadius: '5px',
+    border: "solid black 1px",
 })
 
 // Component Definition
 const Carousel = () => {
-    const [location, setLocation] = useState(0);
+    const [location, setLocation] = React.useState(0);
 
     const picArr = [picOne, picTwo, picThree];
     const picArrLength = picArr.length;
 
+    const handleLeftClickCTA = () => {
+        let nextLocation = location -1;
+        if(nextLocation < 0){
+            nextLocation = picArrLength - 1;
+        }
+        setLocation(nextLocation);
+    }
+
+    const handleRightClickCTA = () => {
+        let nextLocation = location + 1;
+        if(nextLocation >= picArrLength){
+            nextLocation = 0;
+        }
+        setLocation(nextLocation);
+    }
 
 
     return (
         <Wrapper>
-            <StyledButton />
-            <StyledImg />
-            <StyledButton />
+            <CarouselButton onClickCTA={handleLeftClickCTA} >Hello</CarouselButton>
+            <StyledImg src={picArr[location]}/>
+            <CarouselButton onClickCTA={handleRightClickCTA}>{">"}</CarouselButton>
         </Wrapper>
     )
 }
